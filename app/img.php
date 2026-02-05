9405b5946e0588f8470fd7f64c2df681b601f1e9
Hannah A. Patellis <hpatellis@icloud.com>
2026-02-05 10:39:09 -0600

    Adds img.php to test image conversion

---
+++ app/img.php
@@ -0,0 +1,47 @@
+<?php
+
+function getRetroImage($originalPath) {
+    $filename = basename($originalPath);
+    $originalFile = "/var/www/hannahap/public" . $originalPath;
+    
+    // if (!file_exists($originalFile)) return $originalPath;
+    
+    // // Cache key includes file modification time
+    // $mtime = filemtime($originalFile);
+    // $cacheFilename = pathinfo($filename, PATHINFO_FILENAME) . '_' . $mtime . '.gif';
+    // $cachePath = __DIR__ . '/retro-cache/images/' . $cacheFilename;
+    
+    // if (file_exists($cachePath)) {
+    //     return '/retro-cache/images/' . $cacheFilename;
+    // }
+    
+    // Convert image
+    $image = imagecreatefromstring(file_get_contents($originalFile));
+    $width = imagesx($image);
+    $height = imagesy($image);
+    
+    // Resize if needed (max 640px wide)
+    if ($width > 640) {
+        $newWidth = 640;
+        $newHeight = (int)($height * (640 / $width));
+        $resized = imagecreatetruecolor($newWidth, $newHeight);
+        imagecopyresampled($resized, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagedestroy($image);
+        $image = $resized;
+    }
+    
+    // Reduce to 256 colors
+    imagetruecolortopalette($image, true, 256);
+    
+    // Save as GIF
+    imagegif($image, $cachePath);
+    imagedestroy($image);
+    
+    return '/var/www/fs.hannahap/retro-cache/images/' . $cacheFilename;
+}
+
+$result = getRetroImage("/images/zines/petfurpalace_full.jpeg");
+
+print_r($result);
+
+?>
