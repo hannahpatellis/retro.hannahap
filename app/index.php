3647a52bf81bf1e4cfff3b86c888df68e3bf8591
Hannah A. Patellis <hpatellis@icloud.com>
2026-02-05 19:11:38 -0600

    Debugging index.php

--- app/index.php
+++ app/index.php
@@ -1,14 +1,10 @@
 <?php
 
-print_r($_GET['page']);
-
 $page = $_GET['page'] ?? "index";
 if($page === '') {
     $page = "index";
 }
 
-print_r($page);
-
-// header('Location: /retroify.php?page=' . $page);
+header('Location: /retroify.php?page=' . $page);
 
 ?>
